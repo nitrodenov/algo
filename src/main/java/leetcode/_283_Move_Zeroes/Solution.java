023acb7295af95e248ec6d24c897f98ef7d496d5
nitrodenov <nitrodenov@gmail.com>
2020-11-29 21:40:21 +0300

    283. Move Zeroes

---
+++ src/main/java/leetcode/_283_Move_Zeroes/Solution.java
@@ -0,0 +1,18 @@
+package leetcode._283_Move_Zeroes;
+
+/**
+ * [283. Move Zeroes](https://leetcode.com/problems/move-zeroes/)
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
