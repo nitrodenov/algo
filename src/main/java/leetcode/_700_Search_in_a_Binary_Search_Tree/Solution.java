605372768385242124f7df72abd4e2d9223e44cd
nitrodenov <nitrodenov@gmail.com>
2020-12-17 22:05:42 +0300

    700. Search in a Binary Search Tree

---
+++ src/main/java/leetcode/_700_Search_in_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,40 @@
+package leetcode._700_Search_in_a_Binary_Search_Tree;
+
+/**
+ * [700. Search in a Binary Search Tree](https://leetcode.com/problems/search-in-a-binary-search-tree/)
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
