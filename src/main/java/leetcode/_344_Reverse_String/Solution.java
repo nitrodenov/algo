7b2ba53961728e41c042d2ba8662634f22494156
nitrodenov <nitrodenov@gmail.com>
2020-12-17 22:03:50 +0300

    344. Reverse String

--- src/main/java/leetcode/_344_Reverse_String/Solution.java
+++ src/main/java/leetcode/_344_Reverse_String/Solution.java
@@ -5,6 +5,20 @@ package leetcode._344_Reverse_String;
  */
 public class Solution {
     public void reverseString(char[] s) {
+        reverseStringRecursion(s, 0, s.length - 1);
+    }
+
+    private void reverseStringRecursion(char[] s, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        char ch = s[i];
+        s[i] = s[j];
+        s[j] = ch;
+        reverseStringRecursion(s, ++i, --j);
+    }
+
+    public void reverseStringIterative(char[] s) {
         int i = 0;
         int j = s.length - 1;
 
